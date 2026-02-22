166264961d7c0072b3f37e075be6c4910f9441d4
hayley-d <hayleydod@proton.me>
2026-02-22 05:35:30 +0200

    move rust files into rust folder

---
+++ rust-chess/target/debug/deps/chess-359a4f1f7114a347.d
@@ -0,0 +1,9 @@
+/Users/hayley/Projects/chess/rust-chess/target/debug/deps/chess-359a4f1f7114a347.d: src/main.rs Cargo.toml
+
+/Users/hayley/Projects/chess/rust-chess/target/debug/deps/libchess-359a4f1f7114a347.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ rust-chess/target/debug/deps/chess-d8f4cb2201dfd8e8.d
@@ -0,0 +1,9 @@
+/Users/hayley/Projects/chess/rust-chess/target/debug/deps/chess-d8f4cb2201dfd8e8.d: src/main.rs Cargo.toml
+
+/Users/hayley/Projects/chess/rust-chess/target/debug/deps/libchess-d8f4cb2201dfd8e8.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
